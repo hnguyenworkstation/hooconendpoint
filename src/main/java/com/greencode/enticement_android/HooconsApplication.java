e9f18c6ad9c961c8f6c9b01c78a63ac42bd2de4b
Hung Nguyen <hnguyenworkstation@gmail.com>
2017-05-27 09:57:12 -0400

    added more construct

---
+++ src/main/java/com/greencode/enticement_android/HooconsApplication.java
@@ -0,0 +1,12 @@
+package com.greencode.enticement_android;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HooconsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HooconsApplication.class, args);
+	}
+}
