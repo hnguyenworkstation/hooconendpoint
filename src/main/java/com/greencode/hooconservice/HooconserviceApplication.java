37effb80a94b8719d82ebf44341c268dff68f375
Hung Nguyen <hNguyen@IMPRIVATA.com>
2017-05-28 10:04:16 -0400

    added initial project that works

---
+++ src/main/java/com/greencode/hooconservice/HooconserviceApplication.java
@@ -0,0 +1,12 @@
+package com.greencode.hooconservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HooconserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HooconserviceApplication.class, args);
+	}
+}
